4575b417f104216da77ce2cae18f53093f99958e
Valentin Björkli Castillo <valentin.bjorkli@outlook.com>
2025-03-13 12:31:12 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 export default defineConfig({
-	base: '/fed24d-grafiska-verktyg-kubakrokodilerna/',
+	base: '/Valentin-dot-com/Reptilicious',
 	plugins: [vue(), vueDevTools()],
 	resolve: {
 		alias: {
